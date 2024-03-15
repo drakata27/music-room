c2a06a7d14bbcb58571560eb4dd883240e619471
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-03-15 21:33:24 +0000

    Created model and serializer

--- music_room/api/urls.py
+++ music_room/api/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import *
+from django.urls import path
+from .views import RoomView
 
 urlpatterns = [
-    path('', main),
+    path('room', RoomView.as_view()),
 ]
